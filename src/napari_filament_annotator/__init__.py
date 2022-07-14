9be647856950ba61446550060fc2f50504d45f71
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-07-14 15:10:03 -0500

    remove reader

--- src/napari_filament_annotator/__init__.py
+++ src/napari_filament_annotator/__init__.py
@@ -4,13 +4,11 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from ._reader import napari_get_reader
 from ._sample_data import load_sample_image
 from ._widget import ExampleQWidget, example_magic_widget
 from ._writer import write_multiple, write_single_image
 
 __all__ = (
-    "napari_get_reader",
     "write_single_image",
     "write_multiple",
     "load_sample_image",
