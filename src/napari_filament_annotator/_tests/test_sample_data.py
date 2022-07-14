1138250d691e1f53044c5c77263224996a9f909f
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-07-14 14:57:19 -0500

    unit test for sample data

--- src/napari_filament_annotator/_tests/test_sample_data.py
+++ src/napari_filament_annotator/_tests/test_sample_data.py
@@ -1,7 +1,10 @@
-# from napari_filament_annotator import make_sample_data
-
-# add your tests here...
+import numpy as np
+from napari_filament_annotator import load_sample_image
 
 
 def test_something():
-    pass
+    img = load_sample_image()
+    assert isinstance(img, list)
+    assert isinstance(img[0], tuple)
+    assert isinstance(img[0][0], np.ndarray)
+    assert len(img[0][0].shape) == 3
